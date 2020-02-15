9bf73ccfae76c2dbb64e6b9490e1279f83b44f63
Breno Castelo Branco <breno@vizir.com.br>
2020-02-15 17:01:42 -0300

    feat: create orders crud

---
+++ src/app/controllers/OrderController.js
@@ -0,0 +1,107 @@
+import Order from '../models/Order';
+import Recipient from '../models/Recipient';
+import DeliveryMan from '../models/DeliveryMan';
+
+class OrderController {
+  async store(req, res) {
+    const { recipient_id, deliveryman_id } = req.body;
+
+    const recipient = await Recipient.findByPk(recipient_id);
+
+    if (!recipient) {
+      return res.status(404).json({ error: 'Recipient not found.' });
+    }
+
+    const deliveryman = await DeliveryMan.findByPk(deliveryman_id);
+
+    if (!deliveryman) {
+      return res.status(404).json({ error: 'Deliveryman not found.' });
+    }
+
+    const order = await Order.create(req.body);
+
+    return res.json(order);
+  }
+
+  async index(req, res) {
+    const order = await Order.findAll({
+      attributes: [
+        'id',
+        'product',
+        'recipient_id',
+        'deliveryman_id',
+        'canceled_at',
+        'start_date',
+        'end_date',
+      ],
+      include: [
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: ['id', 'name', 'person_id'],
+        },
+        {
+          model: DeliveryMan,
+          as: 'deliveryman',
+          attributes: ['id', 'name', 'email'],
+        },
+      ],
+    });
+
+    return res.json(order);
+  }
+
+  async update(req, res) {
+    const order = await Order.findByPk(req.params.id);
+
+    if (!order) {
+      return res.status(404).json({ error: 'Order not found.' });
+    }
+
+    const { recipient_id, deliveryman_id } = req.body;
+
+    const recipient = await Recipient.findByPk(recipient_id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'The recipient was not found.' });
+    }
+
+    const deliveryman = await DeliveryMan.findByPk(deliveryman_id);
+
+    if (!deliveryman) {
+      return res.status(400).json({ error: 'The deliveryman was not found' });
+    }
+
+    const { id, product } = await order.update(req.body);
+
+    return res.json({ id, product, recipient_id, deliveryman_id });
+  }
+
+  async delete(req, res) {
+    const order = await Order.findByPk(req.params.id, {
+      include: [
+        {
+          model: Recipient,
+          as: 'recipient',
+          attributes: ['id', 'name', 'person_id'],
+        },
+        {
+          model: DeliveryMan,
+          as: 'deliveryman',
+          attributes: ['id', 'name', 'email'],
+        },
+      ],
+    });
+
+    if (!order) {
+      return res.status(404).json({ error: 'Order was not found' });
+    }
+
+    order.canceled_at = new Date();
+    await order.save();
+
+    return res.json(order);
+  }
+}
+
+export default new OrderController();
---
+++ src/app/models/Order.js
@@ -0,0 +1,32 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Order extends Model {
+  static init(connection) {
+    super.init(
+      {
+        product: Sequelize.STRING,
+        canceled_at: Sequelize.DATE,
+      },
+      { sequelize: connection }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Recipient, {
+      foreignKey: 'recipient_id',
+      as: 'recipient',
+    });
+    this.belongsTo(models.DeliveryMan, {
+      foreignKey: 'deliveryman_id',
+      as: 'deliveryman',
+    });
+    this.belongsTo(models.File, {
+      foreignKey: 'signature_id',
+      as: 'signature',
+    });
+  }
+}
+
+export default Order;
